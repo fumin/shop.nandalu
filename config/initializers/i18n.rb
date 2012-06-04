c88c8464e3f65a9c67076da22227c87543ad785f
fumin <awaw@nandalu.idv.tw>
2012-06-05 03:34:14 +0800

    add zh-tw i18n

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,5 +3,6 @@ I18n.default_locale = :en
 
 LANGUAGES = [
   ['English',                  'en'], 
-  ["Espa&ntilde;ol".html_safe, 'es']
+  ["Espa&ntilde;ol".html_safe, 'es'],
+  ["繁體中文".html_safe,       'zh-tw']
 ]
