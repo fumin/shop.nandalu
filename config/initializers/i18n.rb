a47f4b5fdbf89c8c978f41979572c09a81aea3f4
chiefchen <chief_chen@hotmail.com>
2012-06-05 23:27:52 +0800

    Change To Translate & Put logo photo

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,8 @@
 #encoding: utf-8
-I18n.default_locale = :en
+I18n.default_locale = :"zh-tw"
 
 LANGUAGES = [
   ['English',                  'en'], 
-  ["Espa&ntilde;ol".html_safe, 'es']
+  ["Espa&ntilde;ol".html_safe, 'es'],
+  ["繁體中文".html_safe,       'zh-tw']
 ]
